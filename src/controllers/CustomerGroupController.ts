cbe78e5c09266b435223d46a866f51540a580c73
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-06 11:00:17 +0700

    update

--- src/controllers/CustomerGroupController.ts
+++ src/controllers/CustomerGroupController.ts
@@ -319,34 +319,34 @@ class CustomerGroupController implements IController {
 
   show = async (req: Request | any, res: Response): Promise<Response> => {
     try {
-      const cache = await Redis.client.get(`${redisName}-${req.params.id}`);
-      if (cache) {
-        const isCache = JSON.parse(cache);
-        const getHistory = await History.find(
-          {
-            $and: [
-              { "document._id": `${isCache._id}` },
-              { "document.type": redisName },
-            ],
-          },
+      // const cache = await Redis.client.get(`${redisName}-${req.params.id}`);
+      // if (cache) {
+      //   const isCache = JSON.parse(cache);
+      //   const getHistory = await History.find(
+      //     {
+      //       $and: [
+      //         { "document._id": `${isCache._id}` },
+      //         { "document.type": redisName },
+      //       ],
+      //     },
 
-          ["_id", "message", "createdAt", "updatedAt"]
-        )
-          .populate("user", "name")
-          .sort({ createdAt: -1 });
+      //     ["_id", "message", "createdAt", "updatedAt"]
+      //   )
+      //     .populate("user", "name")
+      //     .sort({ createdAt: -1 });
 
-        const buttonActions = await WorkflowController.getButtonAction(
-          redisName,
-          req.userId,
-          isCache.workflowState
-        );
-        return res.status(200).json({
-          status: 200,
-          data: JSON.parse(cache),
-          history: getHistory,
-          workflow: buttonActions,
-        });
-      }
+      //   const buttonActions = await WorkflowController.getButtonAction(
+      //     redisName,
+      //     req.userId,
+      //     isCache.workflowState
+      //   );
+      //   return res.status(200).json({
+      //     status: 200,
+      //     data: JSON.parse(cache),
+      //     history: getHistory,
+      //     workflow: buttonActions,
+      //   });
+      // }
       const result: any = await Db.findOne({ _id: req.params.id })
         .populate("branch", "name")
         .populate("createdBy", "name");
@@ -356,6 +356,8 @@ class CustomerGroupController implements IController {
         req.userId,
         result.workflowState
       );
+
+      const coba = await result.parent
 
       const getHistory = await History.find(
         {
@@ -376,7 +378,7 @@ class CustomerGroupController implements IController {
       
       return res.status(200).json({
         status: 200,
-        data: result,
+        data: coba,
         history: getHistory,
         workflow: buttonActions,
       });

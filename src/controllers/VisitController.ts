88ceda951ee30763625f4043b8bdab9f0d487ac3
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-12 23:23:06 +0700

    update

--- src/controllers/VisitController.ts
+++ src/controllers/VisitController.ts
@@ -117,7 +117,7 @@ class VistController implements IController {
       const order_by: any = req.query.order_by
         ? JSON.parse(`${req.query.order_by}`)
         : { updatedAt: -1 };
-      const limit: number | string = parseInt(`${req.query.limit}`) || 0;
+      const limit: number | string = parseInt(`${req.query.limit}`) || 10;
       let page: number | string = parseInt(`${req.query.page}`) || 1;
       let setField = FilterQuery.getField(fields);
       let isFilter = FilterQuery.getFilter(filters, stateFilter);

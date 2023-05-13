2f40bec621a96afcd9ac3bd2d0730affb66fab7e
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-13 09:48:50 +0700

    update

--- src/models/CallSheetNoteModel.ts
+++ src/models/CallSheetNoteModel.ts
@@ -7,7 +7,91 @@ const CallsheetNoteModel = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      unique: true,
+    },
+    callsheet: {
+      _id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        enum: ["in", "out"],
+      },
+      customer: {
+        _id: {
+          type: Schema.Types.ObjectId,
+          required: true,
+        },
+        name: { type: String, required: true },
+        customerGroup: {
+          _id: {
+            type: Schema.Types.ObjectId,
+            required: true,
+          },
+          name: { type: String, required: true },
+          branch: {
+            _id: {
+              type: Schema.Types.ObjectId,
+              required: true,
+            },
+            name: { type: String, required: true },
+          },
+        },
+      },
+      contact: {
+        _id: {
+          type: Schema.Types.ObjectId,
+          required: true,
+        },
+        name: { type: String, required: true },
+        phone: { type: Number, required: true },
+      },
+      rate: {
+        type: Number,
+        default: 0,
+      },
+      createdBy: {
+        _id: {
+          type: Schema.Types.ObjectId,
+          required: true,
+        },
+        name: { type: String, required: true },
+      },
+      schedule: [
+        {
+          _id: { type: Schema.Types.ObjectId, required: true },
+          name: { type: String, required: true },
+          notes: { type: String, required: true },
+          scheduleList: {
+            _id: { type: Schema.Types.ObjectId, required: true },
+            notes: { type: String, required: true },
+          },
+        },
+      ],
+      status: {
+        type: String,
+        enum: ["0", "1", "2"],
+        default: "0",
+        index: true,
+      },
+      workflowState: {
+        type: String,
+        required: true,
+        default: "Draft",
+        index: true,
+      },
+      createdAt: {
+        type: Date,
+        required: true,
+      },
+      updatedAt: {
+        type: Date,
+        required: true,
+      },
     },
 
     createdBy: {
@@ -23,6 +107,6 @@ const CallsheetNoteModel = new mongoose.Schema(
   }
 );
 
-CallsheetNoteModel.index({ name: 1 });
+// CallsheetNoteModel.index({ name: 1 });
 
 export default mongoose.model("callsheetnote", CallsheetNoteModel);

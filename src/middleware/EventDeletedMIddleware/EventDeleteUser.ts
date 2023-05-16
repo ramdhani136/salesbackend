8082660bd2310837e5c16e59705d1d8d8404551d
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-16 15:52:43 +0700

    update

--- src/middleware/EventDeletedMIddleware/EventDeleteUser.ts
+++ src/middleware/EventDeletedMIddleware/EventDeleteUser.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { CallsheetModel } from "../../models";
+import { ObjectId } from "mongodb";
 
 export const EventDeleteUser = async (
   req: Request,
@@ -6,6 +8,17 @@ export const EventDeleteUser = async (
   next: NextFunction,
   id: String
 ): Promise<any> => {
-  console.log(id);
-  res.send('dd')
+  // Cek Callsheet
+  const callsheet = await CallsheetModel.findOne({
+    "createdBy._id": new ObjectId(`${id}`),
+  });
+  if (callsheet) {
+    return res.status(400).json({
+      status: 404,
+      data: "Error , User terelasi kedalam data callsheet",
+    });
+  }
+  // End Cek Callsheet
+
+  return next();
 };

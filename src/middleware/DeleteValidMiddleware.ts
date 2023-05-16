6d99b78163a5b256b682d11780af66892fd2231f
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-16 15:29:21 +0700

    update

--- src/middleware/DeleteValidMiddleware.ts
+++ src/middleware/DeleteValidMiddleware.ts
@@ -7,8 +7,17 @@ const DeletedValidMiddleware = async (
 ): Promise<any> => {
   try {
     const path = req.path.replace(/\//g, "");
-    console.log(req.baseUrl);
-    console.log(path);
+    let doc: string = req.baseUrl.substring(1);
+
+    switch (doc) {
+      case "users":
+        console.log(req.baseUrl);
+        break;
+
+      default:
+        next();
+        break;
+    }
   } catch (error) {
     return res.status(404).json({ status: 404, data: error });
   }

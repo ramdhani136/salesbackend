4d6656750aaa3b75ff2f059b729cd01351c951c8
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-17 10:39:34 +0700

    update:

--- src/models/VisitNoteModel.ts
+++ src/models/VisitNoteModel.ts
@@ -59,6 +59,46 @@ const VisitNoteModel = new mongoose.Schema(
             _id: { type: Schema.Types.ObjectId, required: true },
             notes: { type: String, required: true },
           },
+          type: {
+            type: String,
+            required: true,
+            enum: ["visit", "callsheet"],
+          },
+          userGroup: {
+            _id: {
+              type: Schema.Types.ObjectId,
+              required: true,
+            },
+            name: {
+              type: String,
+              required: true,
+            },
+          },
+          activeDate: {
+            type: Date,
+            required: true,
+          },
+          closingDate: {
+            type: Date,
+            require: true,
+          },
+          status: {
+            type: String,
+            enum: ["0", "1", "2"],
+            default: "0",
+          },
+          workflowState: {
+            type: String,
+            required: true,
+            default: "Draft",
+          },
+          createdBy: {
+            _id: {
+              type: Schema.Types.ObjectId,
+              required: true,
+            },
+            name: { type: String },
+          },
         },
       ],
       createdAt: {

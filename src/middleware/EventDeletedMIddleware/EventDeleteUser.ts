cbf7d3dc6c484ff371b840c45e058385f0f45b86
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-17 08:49:47 +0700

    update

--- src/middleware/EventDeletedMIddleware/EventDeleteUser.ts
+++ src/middleware/EventDeletedMIddleware/EventDeleteUser.ts
@@ -3,11 +3,36 @@ import {
   BranchModel,
   CallSheetNoteModel,
   CallsheetModel,
+  ContactModel,
+  CustomerGroupModel,
+  CustomerModel,
+  History,
+  MemoModel,
+  PermissionModel,
+  RoleListModel,
+  RoleProfileModel,
+  RoleUserModel,
+  ScheduleListModel,
+  ScheduleModel,
+  TagModel,
+  UserGroupListModel,
+  UserGroupModel,
   VisitNoteModel,
+  Workflow,
+  WorkflowAction,
+  WorkflowChanger,
+  WorkflowState,
+  WorkflowTransition,
   visitModel,
 } from "../../models";
 
 import { CheckData } from "../DeleteValidMiddleware";
+
+interface IData {
+  model: any;
+  doc: string;
+  filters: string[];
+}
 
 export const EventDeleteUser = async (
   req: Request,
@@ -15,44 +40,122 @@ export const EventDeleteUser = async (
   next: NextFunction,
   id: String
 ): Promise<any> => {
-  // Branch
-  if (await CheckData(req, res, BranchModel, "Branch", ["createdBy"])) {
-    return;
-  }
-  // End branch
+  const data: IData[] = [
+    { doc: "Branch", filters: ["createdBy"], model: BranchModel },
+    { doc: "Callsheet", filters: ["createdBy._id"], model: CallsheetModel },
+    {
+      doc: "CallsheetNote",
+      filters: ["callsheet.createdBy._id"],
+      model: CallSheetNoteModel,
+    },
+    { doc: "Visit", filters: ["createdBy._id"], model: visitModel },
+    {
+      doc: "VisitNote",
+      filters: ["visit.createdBy._id"],
+      model: VisitNoteModel,
+    },
+    {
+      doc: "Contact",
+      filters: ["createdBy._id"],
+      model: ContactModel,
+    },
+    {
+      doc: "Customer",
+      filters: ["createdBy._id"],
+      model: CustomerModel,
+    },
+    {
+      doc: "Memo",
+      filters: ["createdBy._id"],
+      model: MemoModel,
+    },
+    {
+      doc: "CustomerGroup",
+      filters: ["createdBy"],
+      model: CustomerGroupModel,
+    },
+    {
+      doc: "History",
+      filters: ["user"],
+      model: History,
+    },
+    {
+      doc: "Permission",
+      filters: ["user", "createdBy"],
+      model: PermissionModel,
+    },
+    {
+      doc: "Rolelist",
+      filters: ["createdBy"],
+      model: RoleListModel,
+    },
+    {
+      doc: "RoleProfile",
+      filters: ["createdBy"],
+      model: RoleProfileModel,
+    },
+    {
+      doc: "RoleUser",
+      filters: ["user", "createdBy"],
+      model: RoleUserModel,
+    },
+    {
+      doc: "ScheduleList",
+      filters: ["schedule.createdBy._id", "createdBy._id"],
+      model: ScheduleListModel,
+    },
+    {
+      doc: "Schedule",
+      filters: ["createdBy._id"],
+      model: ScheduleModel,
+    },
+    {
+      doc: "Tag",
+      filters: ["createdBy._id"],
+      model: TagModel,
+    },
+    {
+      doc: "UserGroupList",
+      filters: ["createdBy._id", "user._id"],
+      model: UserGroupListModel,
+    },
+    {
+      doc: "UserGroup",
+      filters: ["createdBy._id"],
+      model: UserGroupModel,
+    },
+    {
+      doc: "Workflow",
+      filters: ["user"],
+      model: Workflow,
+    },
+    {
+      doc: "WorkflowAction",
+      filters: ["user"],
+      model: WorkflowAction,
+    },
+    {
+      doc: "WorkflowChanger",
+      filters: ["user"],
+      model: WorkflowChanger,
+    },
+    {
+      doc: "workflowState",
+      filters: ["user"],
+      model: WorkflowState,
+    },
+    {
+      doc: "workflowTransition",
+      filters: ["user"],
+      model: WorkflowTransition,
+    },
+  ];
 
-  // Callsheet
-  if (
-    await CheckData(req, res, CallsheetModel, "Callsheet", ["createdBy._id"])
-  ) {
-    return;
+  for (const i of data) {
+    if (await CheckData(req, res, i.model, i.doc, i.filters)) {
+      return;
+    }
   }
-
-  // End Callsheet
-
-  // CallsheetNote
-  if (
-    await CheckData(req, res, CallSheetNoteModel, "CallsheetNote", [
-      "createdBy._id",
-    ])
-  ) {
-    return;
-  }
-  // End CallsheetNote
-
-  // Visit
-  if (await CheckData(req, res, visitModel, "Visit", ["createdBy._id"])) {
-    return;
-  }
-  // End Visit
-
-  //  VisitNote
-  if (
-    await CheckData(req, res, VisitNoteModel, "VisitNote", ["createdBy._id"])
-  ) {
-    return;
-  }
-  // End  VisitNote
 
   // return next();
 };

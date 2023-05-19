f6cb2097942e8195c57f08caefbd1deb30e63d01
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-19 11:09:26 +0700

    update:

--- src/models/CustomerModel.ts
+++ src/models/CustomerModel.ts
@@ -5,7 +5,7 @@ const CustomerModel = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      // unique: true,
+      unique: true,
     },
     type: {
       type: String,

322d06672dbca82bdac6301e629387a94b757f25
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-05-31 11:19:16 +0700

    update

--- src/controllers/CallsheetController.ts
+++ src/controllers/CallsheetController.ts
@@ -30,63 +30,7 @@ import { ISearch } from "../utils/FilterQuery";
 
 const redisName = "callsheet";
 
-// interface dataI {
-//   user?: any;
-//   customer?: any;
-//   branch?: any;
-//   group?: any;
-// }
-
 class CallsheetController implements IController {
-  // protected cekValidPermission = async (
-  //   userId: string,
-  //   dataCheck: dataI,
-  //   doc: selPermissionType
-  // ): Promise<boolean> => {
-  //   const ListData: any = [
-  //     {
-  //       documentCheck: selPermissionAllow.USER,
-  //       data: dataCheck.user,
-  //     },
-  //     {
-  //       documentCheck: selPermissionAllow.BRANCH,
-  //       data: dataCheck.branch,
-  //     },
-  //     {
-  //       documentCheck: selPermissionAllow.CUSTOMER,
-  //       data: dataCheck.customer,
-  //     },
-  //     {
-  //       documentCheck: selPermissionAllow.CUSTOMERGROUP,
-  //       data: dataCheck.group,
-  //     },
-  //   ];
-
-  //   for (const item of ListData) {
-  //     // Mengambil rincian permission user
-  //     if (item.data) {
-  //       const checkData = await PermissionMiddleware.getPermission(
-  //         userId,
-  //         item.documentCheck,
-  //         doc
-  //       );
-
-  //       if (checkData.length > 0) {
-  //         const cekValid = checkData.find(
-  //           (i) => i.toString() === item.data.toString()
-  //         );
-
-  //         if (!cekValid) {
-  //           return false;
-  //         }
-  //       }
-  //     }
-  //     // End
-  //   }
-
-  //   return true;
-  // };
-
   index = async (req: Request | any, res: Response): Promise<Response> => {
     const stateFilter: IStateFilter[] = [
       {
@@ -127,12 +71,7 @@ class CallsheetController implements IController {
         operator: ["=", "!="],
         typeOf: TypeOfState.String,
       },
-      // {
-      //   alias: "CustomerName",
-      //   name: "customer.name",
-      //   operator: ["=", "!=", "like", "notlike"],
-      //   typeOf: TypeOfState.String,
-      // },
+
       {
         alias: "CustomerType",
         name: "customer.type",

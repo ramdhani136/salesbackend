87fd23fdca0902766d537cc4faa04efb736bd463
ramdhani136 <ramdhaniit@gmail.com>
2024-03-01 11:17:29 +0700

    update

--- src/controllers/NotesController.ts
+++ src/controllers/NotesController.ts
@@ -147,6 +147,13 @@ class NotesController implements IController {
         isSort: true,
       },
       {
+        alias: "Schedule",
+        name: "schedule.name",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+        isSort: true,
+      },
+      {
         alias: "Response",
         name: "response",
         operator: ["like", "notlike"],

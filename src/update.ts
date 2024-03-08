d0e6e2911eade0b1f6a33dd55b64d0b1b68f95b3
ramdhani136 <ramdhaniit@gmail.com>
2024-03-08 11:39:18 +0700

    update

--- src/update.ts
+++ src/update.ts
@@ -1,5 +1,14 @@
-import { CallsheetModel, ConfigModel, NotesModel, visitModel } from "./models";
+import {
+  CallsheetModel,
+  ConfigModel,
+  CustomerModel,
+  NotesModel,
+  visitModel,
+} from "./models";
 import mongoose, { ConnectOptions } from "mongoose";
+import * as XLSX from "xlsx";
+import * as fs from "fs";
+import { ObjectId } from "mongodb";
 
 const connection = async () => {
   const options = {
@@ -10,6 +19,7 @@ const connection = async () => {
   mongoose.connect(
     // `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`,
     `mongodb://127.0.0.1:27017/salesapp`,
+    // `mongodb://it:!Etms000!@193.203.162.240:27017/salesapp1?authSource=admin`,
     options as ConnectOptions
   );
 
@@ -25,7 +35,7 @@ const generateCallTypeNotes = async () => {
     "status",
     "workflowState",
     "name",
-    "createdAt"
+    "createdAt",
   ]);
   for (const item of data) {
     await NotesModel.updateMany(
@@ -50,7 +60,7 @@ const generateVisitType = async () => {
     "checkIn",
     "checkOut",
     "name",
-    "createdAt"
+    "createdAt",
   ]);
   for (const item of data) {
     let data: any = {
@@ -73,13 +83,58 @@ const generateVisitType = async () => {
   }
 };
 
+const GenerateItem = async () => {
+  try {
+    const namaFile = "generateitem.xlsx";
+    // Baca file Excel
+    const workbook = XLSX.readFile(namaFile);
+
+    // Ambil nama dari setiap sheet
+    // const sheetNames = workbook.SheetNames;
+
+    const dataUbah: any[] = XLSX.utils.sheet_to_json(workbook.Sheets["ubah"]);
+
+    for (const ubah of dataUbah) {
+      const customer = await CustomerModel.findOne({ name: ubah.id }, [
+        { name: 1 },
+      ]);
+
+      if (!customer) {
+        console.log(ubah.name + " Tidak ditemukan!");
+      } else {
+        const data = {
+          name: ubah.name,
+          customerGroup: new ObjectId(ubah.group),
+          workflowState: ubah.worflowState,
+        };
+        await CustomerModel.updateOne({ name: ubah.id }, data);
+        console.log(ubah.name + " Sukses");
+      }
+    }
+
+    // sheetNames.forEach(sheetName => {
+    //   // Ambil sheet yang diinginkan
+    //   const sheet = workbook.Sheets[sheetName];
+
+    //   // Konversi sheet ke dalam bentuk JSON
+    //   const data = XLSX.utils.sheet_to_json(sheet);
+
+    //   console.log(`Isi dari sheet ${sheetName}:`);
+    //   console.log(data);
+    // });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const run = async () => {
   await connection();
-  console.log("Callsheet");
-  await generateCallTypeNotes();
-  console.log("Visit");
-  await generateVisitType();
-  console.log("selesai");
+  GenerateItem();
+  // console.log("Callsheet");
+  // await generateCallTypeNotes();
+  // console.log("Visit");
+  // await generateVisitType();
+  // console.log("selesai");
 
   // await ConfigModel.updateMany(
   //   {},
